cb419a849abf0f81533bdd4572d93e1af40dae32
Julien Bisconti <julien.bisconti@gmail.com>
2016-01-02 22:29:18 +0100

    improve usage start.sh

--- start.sh
+++ start.sh
@@ -16,9 +16,9 @@ checkDep() {
 
    echo "Missing command: egrep.... exiting"
    echo "Check your \$PATH or try to install it"
-   echo "${bold}sudo apt-get install grep{reset}"
+   echo "${bold}sudo apt-get install grep${reset}"
    echo "For Debian or for CentOS:"
-   echo "${bold}sudo yum install grep{reset}"
+   echo "${bold}sudo yum install grep${reset}"
    exit
   fi
 
@@ -55,18 +55,19 @@ checkDep() {
 usage() {
 cat<<EOF
 
-     ./start.sh [url] [time]
-        * ${bold}${blue}url${reset}:  [URI], must be escaped,
+     ${bold}./start.sh ${underline}[url]${reset} ${underline}[time]${reset}
+        * ${bold}${blue}url${reset}:  (URI), must be escaped,
             This url can be found when installing your script as a webtask.
-            To install your webtask: see #(github_readme_url)
+            To install your webtask: see ${underline}https://github.com/veggiemonk/webtask-twitter#how-to-install-the-webtask-${reset}
             ex: ${tan}https://webtask.it.auth0.com/api/run/(webtask_ID)/webtask${red}\?webtask_no_cache\=1${reset}
-        * ${bold}${blue}time${reset}: [Number], optional, expressed in seconds ( default to 600 = 10min)
+        * ${bold}${blue}time${reset}: (Number), optional, expressed in seconds ( default to 600 = 10min)
 
 
-    To run the script every 10 minutes :
-          ${bold}> ./start.sh ${underline}https://webtask....${reset}
-    To run the script every hours :
-          ${bold}> ./start.sh ${underline}https://webtask.... ${red}3600 ${reset}
+     To run the script every 10 minutes :
+        ${bold}./start.sh ${underline}https://webtask....${reset}
+
+     To run the script every hour :
+        ${bold}./start.sh ${underline}https://webtask.... ${blue}3600${reset}
 EOF
 exit 1
 }
@@ -93,12 +94,12 @@ main() {
   while true; do
     curl --silent "$1" | jq '.[]' | \
     sed 's/\&amp;/\&/g' | \
-    GREP_COLORS='mt=01;32' grep --color=always -E '@\S+|' | \
-    GREP_COLORS='mt=01;35' grep --color=always -E '#\S+|' | \
-    GREP_COLORS='mt=01;33' grep --color=always -E 'http(s){0,1}:\/\/?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\S+|' | \
-    GREP_COLORS='mt=01;31' grep --color=always -E 'http(s){0,1}:\/\/?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\S+\.(jpg|png|jpeg)|' | \
-    GREP_COLORS='mt=01;36' grep --color=always -E 'https:\/\/twitter.com\/?\S+|' | \
-    GREP_COLORS='mt=01;37' grep --color=always -E 'https:\/\/github.com\/?\S+|' | \
+    GREP_COLORS='mt=01;32' grep --color=always -E '@\S+|' | \ #twitter account
+    GREP_COLORS='mt=01;35' grep --color=always -E '#\S+|' | \ #hashtag
+    GREP_COLORS='mt=01;33' grep --color=always -E 'http(s){0,1}:\/\/?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\S+|' | \ #links
+    GREP_COLORS='mt=01;31' grep --color=always -E 'http(s){0,1}:\/\/?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\S+\.(jpg|png|jpeg)|' | \ #pictures
+    GREP_COLORS='mt=01;36' grep --color=always -E 'https:\/\/twitter.com\/?\S+|' | \ #twitter links
+    GREP_COLORS='mt=01;37' grep --color=always -E 'https:\/\/github.com\/?\S+|' | \ # github repo
     emojify;
     sleep "${2:-600}";
   done
